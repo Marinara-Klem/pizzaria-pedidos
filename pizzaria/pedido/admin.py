2a3bca6929588e6cc927b6c10c594f785cd3c752
Marinara-Klem <marinaraklem@gmail.com>
2021-05-12 09:32:45 -0400

    Configurei painel administrativo do app pizza e pedido

--- pizzaria/pedido/admin.py
+++ pizzaria/pedido/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from pedido.models import Pedido, Item
 
 # Register your models here.
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['cliente']
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['quantidade', 'pedido', 'pizza']
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Item, ItemAdmin)
--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from pizza.models import Pizza
 
 # Register your models here.
+
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['sabor', 'preco']
+
+admin.site.register(Pizza, PizzaAdmin)
